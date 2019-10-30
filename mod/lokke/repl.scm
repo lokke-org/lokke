cdff4371c540e4b7594ed1599b0dd4acfd668a02
Rob Browning <rlb@defaultvalue.org>
2019-10-29 19:54:26 -0500

    repl: print result values via prn, not write
    Change the ./lokke repl printer to prn.  I'd hoped to find some easy
    way to set the printer when launching a "normal" repl, but I didn't.
    Might have missed it, or might just need to propose some patches
    upstream.  For now, just borrow a bit more code from Guile proper.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- mod/lokke/repl.scm
+++ mod/lokke/repl.scm
@@ -15,14 +15,27 @@
 
 (define-module (lokke repl)
   use-module: ((lokke ns) select: (default-environment))
-  use-module: ((system repl repl) select: (start-repl))
+  use-module: ((lokke pr) select: (prn))
+  use-module: ((system repl common) select: (make-repl repl-option-set!))
+  use-module: ((system repl repl) select: (run-repl))
   export: (repl)
   duplicates: (merge-generics replace warn-override-core warn last))
 
 ;; If upstream is willing to add an initial-module argument to repl,
-;; or equivalent, then we won't need all this.  For now, just do
-;; exactly what the Guile top-repl does, and then changes the module
-;; to the default-environment, i.e. (lokke user).
+;; or equivalent, and some way to set the printer, then we won't need
+;; all this.  For now, just do exactly what the Guile top-repl does,
+;; and then change the module to the default-environment, i.e. (lokke
+;; user).
+
+;; start-repl adapted from the version in Guile 2.2.6 (LGPL 3)
+(define* (start-repl-w-reader #:optional (lang (current-language))
+                              #:key debug reader)
+  ;; ,language at the REPL will update the current-language.  Make
+  ;; sure that it does so in a new dynamic scope.
+  (parameterize ((current-language lang))
+    (let ((repl (make-repl lang debug)))
+      (repl-option-set! repl 'print (lambda (repl x) (prn x)))
+      (run-repl repl))))
 
 ;; call-with-sigint taken from the version in Guile 2.2.6 (LGPL 3)
 ;; FIXME: propose accommodations upstream
@@ -79,7 +92,7 @@
                 (format (current-error-port)
                         "warning: failed to install locale: ~a~%"
                         (strerror (car errno))))))
-       (let ((status (start-repl (current-language))))
+       (let ((status (start-repl-w-reader (current-language))))
          (run-hook exit-hook)
          status))))
 
