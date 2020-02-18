9d52edb2738540b043822c4339bbc388bb82d671
Rob Browning <rlb@defaultvalue.org>
2020-02-17 20:03:47 -0600

    core.epl: incorporate clojure/jvm 1.10.1 merge-with
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- mod/lokke/ns/clojure/core/epl.clj
+++ mod/lokke/ns/clojure/core/epl.clj
@@ -168,6 +168,24 @@
   ([pred coll]
      (filter (complement pred) coll)))
 
+(defn merge-with
+  ;; "Returns a map that consists of the rest of the maps conj-ed onto
+  ;; the first.  If a key occurs in more than one map, the mapping(s)
+  ;; from the latter (left-to-right) will be combined with the mapping in
+  ;; the result by calling (f val-in-result val-in-latter)."
+  ;; {:added "1.0"
+  ;;  :static true}
+  [f & maps]
+  (when (some identity maps)
+    (let [merge-entry (fn [m e]
+			(let [k (key e) v (val e)]
+			  (if (contains? m k)
+			    (assoc m k (f (get m k) v))
+			    (assoc m k v))))
+          merge2 (fn [m1 m2]
+		   (reduce merge-entry (or m1 {}) (seq m2)))]
+      (reduce merge2 maps))))
+
 (defn nthnext
   ;; "Returns the nth next of coll, (seq coll) when n is 0."
   ;; {:added "1.0"
