aaccf8ffb026db8d925b9ef522df0329de859962
Rob Browning <rlb@defaultvalue.org>
2020-02-29 21:01:34 -0600

    map-entry: add assoc
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- mod/lokke/base/map-entry.scm
+++ mod/lokke/base/map-entry.scm
@@ -19,6 +19,7 @@
   #:use-module (oop goops)
   #:use-module ((lokke base collection)
                 #:select (<sequential>
+                          assoc
                           cons
                           first
                           get
@@ -28,7 +29,7 @@
                           seq))
   #:use-module ((lokke pr) #:select (pr-on print-on))
   #:export (<map-entry> key map-entry map-entry? val)
-  #:re-export (first get nth pr-on print-on rest second seq)
+  #:re-export (assoc first get nth pr-on print-on rest second seq)
   #:duplicates (merge-generics replace warn-override-core warn last))
 
 (define-class <map-entry> (<sequential>)
@@ -62,6 +63,13 @@
 (define-method (counted? (entry <map-entry>)) #t)
 (define-method (not-empty (entry <map-entry>)) entry)
 (define-method (contains? (entry <map-entry>) i) (or (= i 0) (= i 1)))
+
+(define-method (assoc (entry <map-entry>) i x)
+  (case i
+    ((0) (map-entry x (slot-ref entry 'v)))
+    ((1) (map-entry (slot-ref entry 'k) x))
+    (else (scm-error 'out-of-range 'get "<map-entry> index out of range: ~a"
+                     (list i) (list i)))))
 
 (define-method (get (entry <map-entry>) i)
   (case i
