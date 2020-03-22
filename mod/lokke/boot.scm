150f7c605f89470f9d2471184f0644ee0d09796b
Rob Browning <rlb@defaultvalue.org>
2020-03-22 14:24:57 -0500

    Add first attempt at more correct clj syntax-quote
    Rework the boot syntax-quote to (like quote now) actually return forms
    that build the reader literals at evaluation time (e.g. for vector,
    hash-map, hash-set...).
    
    Previously something like `[1] in the repl would
    produce (/lokke/reader-vector 1).
    
    While the semantics here may not exactly match Clojure/JVM, they're
    hopefully a lot closer in the relevant ways.
    
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>
    Tested-by: Rob Browning <rlb@defaultvalue.org>

--- mod/lokke/boot.scm
+++ mod/lokke/boot.scm
@@ -17,36 +17,95 @@
 ;; clojure.core, i.e. (lokke ns clojure core).
 
 (define-module (lokke boot)
-  #:use-module ((guile) #:select ((quote . %scm-quote)))
+  ;; To avoid unquote and unquote-splicing, which when available,
+  ;; break the syntax-quote syntax, and might not be terrible for
+  ;; this particular module to be pure anyway.
+  #:pure
+  #:use-module ((guile)
+                #:select ((quote . %scm-quote)
+                          ...
+                          @
+                          car
+                          case
+                          cddr
+                          cdr
+                          cond
+                          cons
+                          datum->syntax
+                          define
+                          define-syntax
+                          else
+                          error
+                          format
+                          lambda
+                          let
+                          list
+                          list?
+                          map
+                          null?
+                          null?
+                          quasisyntax
+                          syntax
+                          syntax->datum
+                          syntax-case
+                          syntax-error
+                          syntax-rules
+                          unsyntax
+                          unsyntax-splicing))
   #:use-module ((lokke ns) #:select (ns))
+  #:use-module ((srfi srfi-1) #:select (append-map take))
   #:export (/lokke/reader-hash-map
             /lokke/reader-hash-set
             /lokke/reader-vector
             syntax-quote)
-  #:re-export (ns quasiquote unquote unquote-splicing)
-  #:replace (quote)
+  #:re-export (ns)
+  #:replace (quote unquote unquote-splicing)
   #:duplicates (merge-generics replace warn-override-core warn last))
 
 
-(define-syntax-rule (syntax-quote form)
-  (quasiquote form))
+(define (convert-for-public-message expr)
+  (define (convert expr)
+    (cond
+     ((null? expr) expr)
+     ((list? expr)
+      (write (list #:rewriting expr))
+      (newline)
+      (case (car expr)
+        ((/lokke/reader-hash-map) (cons (%scm-quote hash-map) (map convert (cdr expr))))
+        ((/lokke/reader-hash-set) (cons (%scm-quote hash-set) (map convert (cdr expr))))
+        ((/lokke/reader-vector) (cons (%scm-quote vector) (map convert (cdr expr))))
+        (else (map convert-for-public-message expr))))
+     (else expr)))
+  (convert expr))
 
+(define-syntax synerr
+  (syntax-rules ()
+    ((_ name exp msg)
+     (error (format #f "~s: ~a in form ~s" name msg
+                    (convert-for-public-message (syntax->datum exp)))))))
 
 ;; If we eventually have a lower-level module for vector, hash-map,
 ;; and hash-set (to avoid circular references via ns, etc.), we could
 ;; just use-module above and avoid needing the direct @ refs here.
 
-(define-syntax-rule (/lokke/reader-hash-map x ...)
-  ((@ (lokke hash-map) hash-map) x ...))
+(define-syntax /lokke/reader-hash-map
+  (syntax-rules ()
+    ((_ x ...) ((@ (lokke hash-map) hash-map) x ...))))
 
-(define-syntax-rule (/lokke/reader-hash-set x ...)
-  ((@ (lokke hash-set) hash-set) x ...))
+(define-syntax /lokke/reader-hash-set
+  (syntax-rules ()
+    ((_ x ...) ((@ (lokke hash-set) hash-set) x ...))))
 
-(define-syntax-rule (/lokke/reader-vector x ...)
-  ((@ (lokke vector) vector) x ...))
+(define-syntax /lokke/reader-vector
+  (syntax-rules ()
+    ((_ x ...) ((@ (lokke vector) vector) x ...))))
 
 
 (define-syntax quote
+  ;; Note that ~ and ~@ (i.e. unquote and unquote-splicing) still
+  ;; expand symbols inside quoted forms, matching the JVM, but that's
+  ;; handled by the reader.
+  ;;
   ;; FIXME: could perhaps rewrite to scan, and just %scm-quote the whole value
   ;; if the form really is "const", i.e. has no internal maps/sets/vectors.
   (lambda (x)
@@ -67,3 +126,103 @@
 
       ;; "leaf" value, including ()
       ((_ x) #'(%scm-quote x)))))
+
+
+(define-syntax unquote
+  (syntax-rules ()
+    ((_ x ...) (syntax-error "invocation outside syntax-quote"))))
+
+(define-syntax unquote-splicing
+  (syntax-rules ()
+    ((_ x ...) (syntax-error "invocation outside syntax-quote"))))
+
+
+(define (pairify-map-entries ctx exps)
+  ;; (a b c d) -> ((/lokke/reader-vector a b) ...)
+  (let ((rvec-syn (datum->syntax ctx '/lokke/reader-vector)))
+    (let loop ((rest exps))
+      (cond
+       ((null? rest) '())
+       ((null? (cdr rest)) (error "Can't pairify odd length list" exps))
+       (else (cons (cons rvec-syn (take rest 2))
+                   (loop (cddr rest))))))))
+
+(define-syntax syntax-quote
+  ;; FIXME: could perhaps rewrite to scan, and just %scm-quote the whole value
+  ;; if the form really is "const", i.e. has no internal maps/sets/vectors.
+
+  ;; FIXME:? the jvm checks for even number of hash-map forms, but
+  ;; then writes code that would allow an odd number at runtime,
+  ;; e.g. `{~@(list 1 2)} is rejected, but `{~@(list 1) ~@(list 2 3)}
+  ;; is not, but of course crashes later.
+
+  (lambda (x)
+    (define (synquote sub-syn)
+      (syntax-case sub-syn (/lokke/reader-hash-map
+                            /lokke/reader-hash-set
+                            /lokke/reader-vector
+                            unquote
+                            unquote-splicing)
+
+        ((unquote) (synerr 'unquote x "unquote: no arguments"))
+        ((unquote exp) #'(exp))
+
+        ((unquote-splicing) (synerr 'unquote-splicing x "unquote: no arguments"))
+        ((unquote-splicing (/lokke/reader-vector exp ...)) #'(exp ...))
+        ((unquote-splicing (/lokke/reader-hash-set exp ...)) #'(exp ...))
+
+        ((unquote-splicing (/lokke/reader-hash-map exp ...))
+         (pairify-map-entries #'sub-syn #'(exp ...)))
+
+        ((unquote-splicing exp) #'(exp))
+
+        ((/lokke/reader-vector exp ...)
+         #`(/lokke/reader-vector
+            #,@(map (lambda (e) #`(syntax-quote #,e))
+                    #'(exp ...))))
+
+        ((/lokke/reader-hash-map exp ...)
+         #`(/lokke/reader-hash-map
+            #,@(map (lambda (e) #`(syntax-quote #,e))
+                    #'(exp ...))))
+
+        ((/lokke/reader-hash-set exp ...)
+         #`(/lokke/reader-hash-set
+            #,@(map (lambda (e) #`(syntax-quote #,e))
+                    #'(exp ...))))
+
+        ((exp ...)
+         #`((list
+             #,@(map (lambda (e) #`(syntax-quote #,e))
+                     #'(exp ...)))))
+
+        ;; "leaf" value, including ()
+        (x #'((quote x)))))
+
+    (syntax-case x (/lokke/reader-hash-map
+                    /lokke/reader-hash-set
+                    /lokke/reader-vector
+                    unquote
+                    unquote-splicing)
+
+      ((_ (unquote)) (synerr 'unquote x "no arguments"))
+      ((_ (unquote exp)) #'exp)
+
+      ((_ (unquote-splicing)) (synerr 'unquotes-splicing x "no arguments"))
+      ((_ (unquote-splicing exp ...))
+       (synerr 'unquote-splicing x "not inside list"))
+
+      ((_ (/lokke/reader-vector exp ...))
+       #`(/lokke/reader-vector #,@(append-map synquote #'(exp ...))))
+
+      ((_ (/lokke/reader-hash-map exp ...))
+       #`(/lokke/reader-hash-map #,@(append-map synquote #'(exp ...))))
+
+      ((_ (/lokke/reader-hash-set exp ...))
+       #`(/lokke/reader-hash-set #,@(append-map synquote #'(exp ...))))
+
+      ((_ (exp ...))
+       #`(list #,@(append-map synquote #'(exp ...))))
+
+      ;; "leaf" value, including ()
+      ((_ x) #'(quote x)))))
