3a061d74e7dc94f046cda2f3908b5090d813212d
Rob Browning <rlb@defaultvalue.org>
2021-01-23 12:15:53 -0600

    core.epl: add clojure/jvm 1.10.1 pmap pcalls pvalues
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- mod/lokke/ns/clojure/core/epl.clj
+++ mod/lokke/ns/clojure/core/epl.clj
@@ -228,6 +228,46 @@
                         (mapcat walk (children node))))))]
     (walk root)))
 
+(defn pmap
+  "Like map, except f is applied in parallel. Semi-lazy in that the
+  parallel computation stays ahead of the consumption, but doesn't
+  realize the entire result unless required. Only useful for
+  computationally intensive functions where the time of f dominates
+  the coordination overhead."
+  {:added "1.0"
+   :static true}
+  ([f coll]
+   (let [n (+ 2 (guile.ice-9.threads/current-processor-count))
+         rets (map #(future (f %)) coll)
+         step (fn step [[x & xs :as vs] fs]
+                (lazy-seq
+                 (if-let [s (seq fs)]
+                   (cons (deref x) (step xs (rest s)))
+                   (map deref vs))))]
+     (step rets (drop n rets))))
+  ([f coll & colls]
+   (let [step (fn step [cs]
+                (lazy-seq
+                 (let [ss (map seq cs)]
+                   (when (every? identity ss)
+                     (cons (map first ss) (step (map rest ss)))))))]
+     (pmap #(apply f %) (step (cons coll colls))))))
+
+(defn pcalls
+  "Executes the no-arg fns in parallel, returning a lazy sequence of
+  their values"
+  {:added "1.0"
+   :static true}
+  [& fns] (pmap #(%) fns))
+
+(defmacro pvalues
+  ;; "Returns a lazy sequence of the values of the exprs, which are
+  ;; evaluated in parallel"
+  ;; {:added "1.0"
+  ;;  :static true}
+  [& exprs]
+  `(pcalls ~@(map #(list `fn [] %) exprs)))
+
 (defn flatten
   "Takes any nested combination of sequential things (lists, vectors,
   etc.) and returns their contents as a single, flat lazy sequence.
