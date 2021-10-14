bd8483108df40ebfcb62d429ea137114bf15e85e
Rob Browning <rlb@defaultvalue.org>
2021-10-14 00:28:08 -0500

    clojure.test: support (is (thrown? ...))
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- mod/lokke/ns/clojure/test.scm
+++ mod/lokke/ns/clojure/test.scm
@@ -1,4 +1,4 @@
-;;; Copyright (C) 2019 Rob Browning <rlb@defaultvalue.org>
+;;; Copyright (C) 2019-2021 Rob Browning <rlb@defaultvalue.org>
 ;;; SPDX-License-Identifier: LGPL-2.1-or-later OR EPL-1.0+
 
 (define-module (lokke ns clojure test)
@@ -8,6 +8,7 @@
                           test-begin
                           test-end
                           test-equal
+                          test-error
                           test-group
                           test-runner-current
                           test-runner-fail-count))
@@ -27,7 +28,7 @@
 
 (define-syntax testing
   (syntax-rules ()
-    ((_ what test ...) (test-group what test ...))))
+    ((_ what test ...) (test-group what (begin #t test ...)))))
 
 ;; FIXME: support *load-tests*?
 ;; FIXME: deftest should of course not be executing the code immediately
@@ -48,11 +49,12 @@
 
 ;; For now just supports (is x) and (is (= x y)), since that's easy to
 ;; do with srfi-64, but this will almost certainly need an overhaul,
-;; and/or moving away from the srfi.
+;; and/or moving away from the srfi, and mere syntax pattern matching.
 
 (define-syntax is
-  (syntax-rules (=)
+  (syntax-rules (= thrown?) ;; FIXME: change literals to explicit pattern guards?
     ((_ (= expected expression)) (test-equal expected expression))
     ((_ (= expected expression) msg) (test-equal msg expected expression))
+    ((_ (thrown? ex-type body ...)) (test-error ex-type body ...))
     ((_ expression) (test-assert expression))
     ((_ expression msg) (test-assert msg expression))))
